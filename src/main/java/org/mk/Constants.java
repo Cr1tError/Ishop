9f6e8142c21869233f7ad89e653836b7f1b1e5cc
Cr1tError <maksym.kokora@gmail.com>
2022-03-31 12:51:33 +0200

    create cookie enum to use it in WebUtils class.

--- src/main/java/org/mk/Constants.java
+++ src/main/java/org/mk/Constants.java
@@ -7,4 +7,24 @@ public final class Constants {
 
     public static final int MAX_PRODUCTS_PER_SHOPPING_CART = 20;
 
+    public enum Cookie {
+        //1 year ttl
+        SHOPPING_CART("iSCC", 60 * 60 * 24 * 365);
+
+        private final String name;
+        private final int ttl;
+
+        private Cookie(String name, int ttl) {
+            this.name = name;
+            this.ttl = ttl;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public int getTtl() {
+            return ttl;
+        }
+    }
 }
