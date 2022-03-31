d766d685b8fe7270bb137e715101bf561741925c
Cr1tError <maksym.kokora@gmail.com>
2022-03-31 13:04:26 +0200

    create method to deserialize shopping cart from cookie.

--- src/main/java/org/mk/filter/AutoRestoreShoppingCartFilter.java
+++ src/main/java/org/mk/filter/AutoRestoreShoppingCartFilter.java
@@ -1,4 +1,26 @@
 package org.mk.filter;
 
+import org.mk.model.ShoppingCart;
+
+import javax.servlet.annotation.WebFilter;
+
+@WebFilter(filterName="AutoRestoreShoppingCartFilter")
 public class AutoRestoreShoppingCartFilter {
+
+
+    protected ShoppingCart shoppingCartFromString(String cookieValue){
+        ShoppingCart shoppingCart = new ShoppingCart();
+        String[] items = cookieValue.split("\\|");
+        for(String  s : items){
+            String[] data = s.split("-");
+            try{
+                int productId = Integer.parseInt(data[0]);
+                int count = Integer.parseInt(data[1]);
+                shoppingCart.addProduct(productId, count);
+            } catch (RuntimeException e ){
+                e.printStackTrace();
+            }
+        }
+        return shoppingCart;
+    }
 }
