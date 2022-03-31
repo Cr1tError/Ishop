986e1bf047bb0c0af420d889b934e8041444b3b2
Cr1tError <maksym.kokora@gmail.com>
2022-03-31 12:26:39 +0200

    create ShoppingCart class and add validation of shopping cart size (cart can contain max 20 product.

---
+++ src/main/java/org/mk/model/ShoppingCart.java
@@ -0,0 +1,21 @@
+package org.mk.model;
+
+import org.mk.Constants;
+import org.mk.exception.ValidationException;
+
+import java.io.Serializable;
+import java.util.HashMap;
+import java.util.Map;
+
+public class ShoppingCart implements Serializable {
+    private Map<Integer, ShoppingCartItem> products = new HashMap<>();
+    private int totalCount = 0;
+
+    private void validateShoppingCartSize(int idProduct){
+        if(products.size() > Constants.MAX_PRODUCTS_PER_SHOPPING_CART ||
+                (products.size() == Constants.MAX_PRODUCTS_PER_SHOPPING_CART && !products.containsKey(idProduct))) {
+            throw new ValidationException("Limit for ShoppingCart size reached: size="+products.size());
+        }
+    }
+
+}
