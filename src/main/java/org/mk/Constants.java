9f44f8bc1248e02aaf4078a6d8f81a792d27e388
Cr1tError <maksym.kokora@gmail.com>
2022-03-31 12:24:14 +0200

    create Constants for save and use constants in other classes.

---
+++ src/main/java/org/mk/Constants.java
@@ -0,0 +1,10 @@
+package org.mk;
+
+public final class Constants {
+    public static final String CURRENT_SHOPPING_CART = "CURRENT_SHOPPING_CART";
+
+    public static final int MAX_PRODUCT_COUNT_PER_SHOPPING_CART = 10;
+
+    public static final int MAX_PRODUCTS_PER_SHOPPING_CART = 20;
+
+}
