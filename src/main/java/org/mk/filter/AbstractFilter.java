8f1e58495862625903176fd2d96fc0856a0e630e
Cr1tError <maksym.kokora@gmail.com>
2022-03-31 11:49:39 +0200

    create abstract filter to using it in the future.

---
+++ src/main/java/org/mk/filter/AbstractFilter.java
@@ -0,0 +1,34 @@
+package org.mk.filter;
+
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+
+
+public abstract class AbstractFilter implements Filter {
+    protected final Logger LOGGER = LoggerFactory.getLogger(getClass());
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+    }
+
+    @Override
+    public final void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletRequest req = (HttpServletRequest) servletRequest;
+        HttpServletResponse resp = (HttpServletResponse) servletResponse;
+        String url = req.getRequestURI();
+        doFilter(req, resp, filterChain);
+
+    }
+
+    @Override
+    public void destroy() {
+    }
+}
