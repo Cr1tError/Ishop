ea741ec47fb9a3cb6f4ee3fb7e59cf9d3209c79c
Cr1tError <maksym.kokora@gmail.com>
2022-03-31 12:35:35 +0200

    create 3 new methods: 1) to return collection  values of products in shopping cart. 2) to add product in shopping cart(with validation for max size of cart and max count of each product). 3)to refresh statistic of total count of products in shopping cart.

--- src/main/java/org/mk/model/ShoppingCart.java
+++ src/main/java/org/mk/model/ShoppingCart.java
@@ -4,12 +4,38 @@ import org.mk.Constants;
 import org.mk.exception.ValidationException;
 
 import java.io.Serializable;
+import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
 
 public class ShoppingCart implements Serializable {
     private Map<Integer, ShoppingCartItem> products = new HashMap<>();
     private int totalCount = 0;
+
+    public void addProduct(int idProduct, int count) {
+        validateShoppingCartSize(idProduct);
+        ShoppingCartItem shoppingCartItem = products.get(idProduct);
+        if (shoppingCartItem == null) {
+            validateProductCount(count);
+            shoppingCartItem = new ShoppingCartItem(idProduct, count);
+            products.put(idProduct, shoppingCartItem);
+        } else {
+            validateProductCount(count + shoppingCartItem.getCount());
+            shoppingCartItem.setCount(shoppingCartItem.getCount() + count);
+        }
+        refreshStatistics();
+    }
+
+    public Collection<ShoppingCartItem> getItems() {
+        return products.values();
+    }
+
+    private void refreshStatistics() {
+        totalCount = 0;
+        for (ShoppingCartItem shoppingCartItem : getItems()) {
+            totalCount += shoppingCartItem.getCount();
+        }
+    }
 
     private void validateShoppingCartSize(int idProduct){
         if(products.size() > Constants.MAX_PRODUCTS_PER_SHOPPING_CART ||
