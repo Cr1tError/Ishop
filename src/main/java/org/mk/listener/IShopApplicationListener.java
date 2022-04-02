ad77859936b28256d4fa48f1839028f0cb7390a5
Cr1tError <maksym.kokora@gmail.com>
2022-04-03 00:19:12 +0200

    create app listener for future.

---
+++ src/main/java/org/mk/listener/IShopApplicationListener.java
@@ -0,0 +1,18 @@
+package org.mk.listener;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+
+
+public class IShopApplicationListener implements ServletContextListener {
+
+    @Override
+    public void contextInitialized(ServletContextEvent sce) {
+
+    }
+
+    @Override
+    public void contextDestroyed(ServletContextEvent sce) {
+
+    }
+}
